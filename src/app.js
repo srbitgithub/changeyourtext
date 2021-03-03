59eed539402bdd6fad2ec72f341cea293c204faf
srbit <sr_bit@hotmail.com>
2021-03-03 19:27:54 +0100

    Installed bodyparser dependence

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const bodyParser = require('body-parser')
 
 app.use(morgan('dev'))
-
+app.use(bodyParser.json());
 app.use(require('./routes/index'))
+
+//app.use(bodyParser.urlencoded({ extended: false }))
 
 module.exports = app
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,19 @@
 const {Router} = require('express')
 const router = Router()
 
+
+
 router.get('/', (req, res) => {
-    res.json({mensaje: 'Hola mundo'})
+    res.json({mensaje: 'Hola mundo get'})
 })
 
-router.post('/change', (req, res) => {
-    res.json({mensaje: 'Hola mundo'})
-})
+// router.post('/change', (req, res) => {
+//     res.json({mensaje: 'Hola mundo'})
+// })
 
+router.post('/', (req, res) => {
+    console.log(req.body)
+    res.json({mensaje: 'Hola mundo post'})
+})
 
 module.exports = router
